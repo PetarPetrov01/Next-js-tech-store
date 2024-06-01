7dd3e1b617bf2bb2778f14a84a6a20758ceebcea
PetarPetrov01 <petyr_p@mail.bg>
2024-06-01 19:19:29 +0300

    Initial about page markup

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+export default function Page() {
+  return (
+    <main>
+      <section className="flex "></section>
+      <div className="container max-w-[1400px] flex justify-center py-5">
+        <div className="flex flex-col items-center">
+            <article>
+                <h2 className="text-4xl">Our prods</h2>
+            </article>
+        </div>
+      </div>
+    </main>
+  );
+}
