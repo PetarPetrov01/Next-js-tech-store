a5a452817fc21426295b5350e7d207063862338c
PetarPetrov01 <petyr_p@mail.bg>
2024-06-05 22:37:59 +0300

    style: about us

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,7 @@ import { Metadata } from "next";
 import { getProds } from "../lib/data";
 
 export const metadata: Metadata = {
-  title: 'About',
+  title: "About",
 };
 
 export default async function Page() {
@@ -19,15 +19,14 @@ export default async function Page() {
               {prods.map((prod) => (
                 <div
                   key={prod._id}
-                  className="flex w-[70%] justify-center gap-4 mt-8 border-[1px] border-purple py-2 px-2 rounded-lg "
+                  className="flex flex-col w-[70%] justify-center gap-2 mt-8 border-[1px] border-lightblue text-lightblue p-3 rounded-lg  bg-blue-700/30 shadow-s shadow-[0_2px_15px_2px_rgba(0,180,216,0.4)]"
                 >
                   <div>
-                    {/* <Image src={prod.image} alt={prod.name} width={200} height={200}/>
-                     */}
                     <h2 className="text-3xl">{prod.name}</h2>
                   </div>
-                  <h2>{prod.category}</h2>
+                  <h2>{prod.category.join(', ')}</h2>
                   <h2>{prod.color}</h2>
+                  <p>{prod.description} </p>
                 </div>
               ))}
             </div>
