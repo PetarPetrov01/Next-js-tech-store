642779b5fbc934bf78bbda830dd3ce9f27637e84
PetarPetrov01 <petyr_p@mail.bg>
2024-06-08 18:56:38 +0300

    feat: product card ui

---
+++ src/app/ui/product-card.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import {} from "next/"
+
+import { Product } from "../types/Product";
+import { useRouter } from "next/router";
+
+export default function ProductCard({ prod }: { prod: Product }) {
+
+const router = useRouter();
+
+  return (
+    <div
+      onClick={() => router.push(`${prod._id}`)}
+      key={prod._id}
+      className="flex flex-col w-[70%] justify-center gap-2 mt-8 border-[1px] border-lightblue text-lightblue p-3 rounded-lg  bg-blue-700/30 shadow-s hover:shadow-[0_2px_15px_2px_rgba(0,180,216,0.4)]"
+    >
+      <div>
+        <h2 className="text-3xl">{prod.name}</h2>
+      </div>
+      <h2>{prod.category.join(", ")}</h2>
+      <h2>{prod.color}</h2>
+      <p>{prod.description} </p>
+    </div>
+  );
+}
