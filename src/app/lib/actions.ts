84c9f27b53a49020dff43d957761270b07108e28
PetarPetrov01 <petyr_p@mail.bg>
2024-06-18 17:16:46 +0300

    register action

--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -65,3 +65,52 @@ export const login = async (prevState: ErrorsState, formData: FormData) => {
   }
   redirect("/");
 };
+
+export const register = async (prevState: ErrorsState, formData: FormData) => {
+  try {
+    const validatedFields = UserRegisterSchema.safeParse({
+      email: formData.get("email"),
+      username: formData.get("username"),
+      password: formData.get("password"),
+      repassword: formData.get("repassword"),
+    });
+
+    if (!validatedFields.success) {
+      return {
+        errors: validatedFields.error.flatten().fieldErrors,
+        message: "Missing fields",
+      };
+    }
+
+    if (validatedFields.data.password != validatedFields.data.repassword) {
+      return {
+        message: "Passwords do not match",
+      };
+    }
+
+    const data = validatedFields.data;
+
+    const res = await fetch("http://localhost:3030/auth/register", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: { "Content-Type": "application/json" },
+      credentials: "same-origin",
+      cache: "no-store",
+    });
+
+    const authCookie = res.headers.getSetCookie()[0];
+
+    if (authCookie && res.statusText == "OK") {
+      const [cookieString, cookiePathString] = authCookie.split("; ");
+      const [cookieName, authToken] = cookieString.split("=");
+      const cookiePath = cookiePathString.split("=")[1];
+
+      cookies().set(cookieName, authToken, { path: cookiePath });
+    }
+    const result = await res.json();
+
+    if (res.statusText != "OK") {
+      throw new Error(result.message);
+    }
+  } catch (error) {}
+};
