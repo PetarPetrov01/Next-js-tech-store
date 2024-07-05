7af6d21f109cb44cf125744d873c50e6f7a1e7c2
PetarPetrov01 <petyr_p@mail.bg>
2024-07-05 16:12:04 +0300

    Prisma setup and test migration

--- Back-end/src/index.ts
+++ Back-end/src/index.ts
@@ -1,12 +1,31 @@
-const { Application } = require('express');
+import { PrismaClient } from "@prisma/client";
+import { Application } from "express";
+import cors from 'cors'
 
-// import express from 'express'
-const express = require('express');
+import express from "express";
 
-const app = express()
+const app: Application = express();
+const prisma = new PrismaClient();
 
-app.get('/',(req,res)=>{
-    res.send('Hello')
-})
+app.use(cors())
 
-app.listen(3000,()=>{console.log('Server listening to 3000')})
+app.get("/", async (req, res) => {
+  const user = await prisma.user.findMany();
+  res.send(user);
+});
+
+app.post("/create", async (req, res) => {
+  const { email, username, password } = req.body;
+  try {
+    const createdUser = await prisma.user.create({
+      data: { email, username, password },
+    });
+    res.send(createdUser);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server listening to 3000");
+});
