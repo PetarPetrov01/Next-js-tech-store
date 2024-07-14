e82ac939689563df08b2e8862d1c2afed5ead7a0
PetarPetrov01 <petyr_p@mail.bg>
2024-07-14 15:58:47 +0300

    Implement saving the image in cloudinary

--- Back-end/src/controllers/upload.controller.ts
+++ Back-end/src/controllers/upload.controller.ts
@@ -1,14 +1,36 @@
 import { Request, Response } from "express";
+import { uploadToCloudinary } from "../utils/cloudinary";
+import fs from "fs";
 
 async function uploadImage(req: Request, res: Response) {
-    if (!req.file) {
-        return res.status(400).send({ message: "No file uploaded." });
-      }
-    
-      res.status(200).send({
-        message: "File uploaded successfully.",
-        file: req.file
-      });
+  if (!req.file) {
+    return res.status(400).send({ message: "No file uploaded." });
+  }
+
+  try {
+    const result = await uploadToCloudinary(req.file.path, "Images");
+    const imageUrl = result.secure_url;
+    console.log(imageUrl);
+
+    fs.unlink(req.file.path,(err)=>{
+        if(err){
+            console.error(err)
+        } else {
+            console.log('Sucessfully deleted from file system')
+        }
+    })
+
+    res.status(200).send({
+      message: "File uploaded successfully.",
+      file: req.file,
+    });
+  } catch (error: any) {
+    console.log(error);
+    res.status(500).send({
+      message: "File upload failed.",
+      error: error.message,
+    });
+  }
 }
 
 const uploadController = { uploadImage };
