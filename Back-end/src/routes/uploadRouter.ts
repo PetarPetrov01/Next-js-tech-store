98eb92b83305c29154a39be5bae98647513db233
PetarPetrov01 <petyr_p@mail.bg>
2024-08-06 14:32:46 +0300

    Remove unnecessary upload validation
    Multer validates the upload itself

--- Back-end/src/routes/uploadRouter.ts
+++ Back-end/src/routes/uploadRouter.ts
@@ -1,13 +1,11 @@
 import express, { Router } from "express";
 import uploadController from "../controllers/upload.controller";
 import upload from "../utils/multer";
-import uploadValidation from "../middlewares/validations/upload-request";
 
 const uploadRouter: Router = express.Router();
 
 uploadRouter.post(
   "/image",
-  uploadValidation,
   upload.single("image"),
   uploadController.uploadImage
 );
