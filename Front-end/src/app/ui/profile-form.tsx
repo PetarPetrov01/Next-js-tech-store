7455fb3c6b184d2732de577e33cba6ec2cbb0cee
PetarPetrov01 <petyr_p@mail.bg>
2024-08-14 15:55:05 +0300

    Restyle profile page

--- Front-end/src/app/ui/profile-form.tsx
+++ Front-end/src/app/ui/profile-form.tsx
@@ -15,18 +15,21 @@ const ImageSchema = z.object({
     .refine((file) => /jpg|png|jpeg/.test(file.name.toLocaleLowerCase()), {
       message: "File type not allowed",
     })
-    .refine((file)=>/image\/png|image\/jpeg|image\/gif|image\/webp/.test(file.type),{message: 'This file is not allowed'})
+    .refine(
+      (file) => /image\/png|image\/jpeg|image\/gif|image\/webp/.test(file.type),
+      { message: "This file is not allowed" }
+    )
     .refine((file) => file.size <= 1024 * 200, {
       message: "File is too large",
-    })
+    }),
 });
 
 type Inputs = z.infer<typeof ImageSchema>;
 
-export default function ProfileForm({user}:{user: User}) {
+export default function ProfileForm({ user }: { user: User }) {
   const [selectedImage, setSelectedImage] = useState<null | string>(null);
 
-  const imageInputRef = useRef< HTMLInputElement| null>( null);
+  const imageInputRef = useRef<HTMLInputElement | null>(null);
   const formRef = useRef<HTMLFormElement>(null);
 
   const router = useRouter();
@@ -38,21 +41,21 @@ export default function ProfileForm({user}:{user: User}) {
   const onImageChange = (event: ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files.length > 0) {
       const file = event.target.files[0];
-      console.log(file.type)
+      console.log(file.type);
       setSelectedImage(file.name);
-      setValue('image',file);
+      setValue("image", file);
     }
   };
 
-  console.log('IMAGE')
-  console.log(user?.image)
+  console.log("IMAGE");
+  console.log(user?.image);
 
   const {
     handleSubmit,
     formState: { errors, dirtyFields },
     reset,
     register,
-    setValue
+    setValue,
   } = useForm<Inputs>({
     resolver: zodResolver(ImageSchema),
   });
@@ -68,13 +71,13 @@ export default function ProfileForm({user}:{user: User}) {
     if (formRef.current) {
       //If new FormData(formRef.current) is used, the file size appears to be 0 !?
       const formData = new FormData();
-      formData.append('image',data.image)
+      formData.append("image", data.image);
       try {
-        const response = await fetch('http://localhost:3001/api/upload/image',{
-          method: 'POST',
+        const response = await fetch("http://localhost:3001/api/upload/image", {
+          method: "POST",
           body: formData,
           credentials: "include",
-        })
+        });
 
         const result = await response.json();
         router.refresh();
@@ -85,31 +88,42 @@ export default function ProfileForm({user}:{user: User}) {
   };
 
   return (
-    <div>
-      <form action="" ref={formRef} encType="multipart/form-data" onSubmit={handleSubmit(processSubmit)}>
-        <article className="relative flex w-full flex-col items-center gap-4">
-          <h2 className="mt-4 text-dark-blue">Profile</h2>
-          <div className="relative rounded-full border-4 border-[#00b4d8cc] shadow-md shadow-[#00b4d866]">
-            <Image
-              src={user?.image ? user.image : "/default-company-pic.png"}
-              alt="profile pic"
-              width={150}
-              height={150}
-              className="rounded-full"
-            />
-            <a className="absolute bottom-1 right-1 cursor-pointer rounded-full bg-gray-200 p-[0.4rem] duration-150 hover:bg-gray-300">
-              <CameraIcon height={20} width={20} onClick={handleImageClick} />
-            </a>
-            <input
-              type="file"
-              className="hidden"
-              ref={(e) => {
-                ref(e)
-                imageInputRef.current = e
-              }}
-              {...rest}
-              onChange={onImageChange}
-            />
+    <>
+      <h2 className="mt-4 text-new-peach text-center">{user.firstName} {user.lastName}</h2>
+      <form
+        action=""
+        ref={formRef}
+        encType="multipart/form-data"
+        onSubmit={handleSubmit(processSubmit)}
+        className="flex flex-col items-center sm:items-start sm:flex-row justify-between sm:gap-5 w-full"
+      >
+        <article className="w-[60%]  sm:w-[35%] relative flex flex-col items-center gap-4 py-10">
+          <div className="relative w-[95%] flex flex-col bg-new-teal pt-28 p-16 rounded-2xl min-h-[20vh]">
+            <div className="absolute top-[-35%] sm:top-[-25%] left-[50%] translate-x-[-50%] rounded-full border-4 border-new-peach shadow-md shadow-new-sandstone">
+              <Image
+                src={user?.image ? user.image : "/default-company-pic.png"}
+                alt="profile pic"
+                width={150}
+                height={150}
+                className="rounded-full aspect-square object-cover"
+              />
+              <a className="absolute bottom-1 right-1 cursor-pointer rounded-full bg-new-gray text-new-peach p-[0.4rem] duration-150 hover:bg-new-teal">
+                <CameraIcon height={20} width={20} onClick={handleImageClick} />
+              </a>
+              <input
+                type="file"
+                className="hidden"
+                ref={(e) => {
+                  ref(e);
+                  imageInputRef.current = e;
+                }}
+                {...rest}
+                onChange={onImageChange}
+              />
+            </div>
+            <h3>{user.username}</h3>
+            <h3>{user.email}</h3>
+            <h3>phone</h3>
           </div>
           {selectedImage && (
             <div className="flex gap-6 items-center border-b-[1px] border-lightblue">
@@ -128,6 +142,12 @@ export default function ProfileForm({user}:{user: User}) {
             </span>
           )}
         </article>
+        <article className="w-[85%] sm:w-[60%] sm:py-10">
+          <div className="bg-new-teal rounded-2xl p-10">
+          <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus repudiandae iusto corrupti labore culpa reiciendis amet id quos est magni.</h3>
+          
+          </div>
+        </article>
         {selectedImage && (
           <div className="flex justify-center mt-8">
             <button type="submit" className="px-4 py-2 bg-[#00b4d888]">
@@ -136,6 +156,6 @@ export default function ProfileForm({user}:{user: User}) {
           </div>
         )}
       </form>
-    </div>
+    </>
   );
 }
--- Front-end/src/app/ui/register-form.tsx
+++ Front-end/src/app/ui/register-form.tsx
@@ -11,6 +11,16 @@ import { useAuthContext } from "@/contexts/AuthProvider";
 
 const UserRegisterSchema = z
   .object({
+    firstName: z
+      .string()
+      .trim()
+      .min(1, { message: "First name is required" })
+      .min(3, { message: "First name must be atleast 3 characters long" }),
+    lastName: z
+      .string()
+      .trim()
+      .min(1, { message: "Last name is required" })
+      .min(3, { message: "Last name must be atleast 3 characters long" }),
     email: z
       .string()
       .trim()
@@ -54,6 +64,8 @@ export default function RegisterForm({ ptSerif }: { ptSerif: NextFont }) {
   } = useForm<Inputs>({
     resolver: zodResolver(UserRegisterSchema),
     defaultValues: {
+      firstName: "",
+      lastName: "",
       email: "",
       password: "",
       repassword: "",
@@ -109,6 +121,32 @@ export default function RegisterForm({ ptSerif }: { ptSerif: NextFont }) {
         <input
           type="text"
           className="text-new-gray rounded-md min-h-9 w-[100%] text-lg border-0 px-3 focus:outline-none focus:outline-[1px] focus:outline-peach"
+          placeholder="First name"
+          {...register("firstName")}
+        />
+        {errors.firstName && (
+          <span className="absolute bottom-[-1.5em]">
+            {errors.firstName.message}
+          </span>
+        )}
+      </div>
+      <div className="relative flex flex-col input-group w-[80%]">
+        <input
+          type="text"
+          className="text-new-gray rounded-md min-h-9 w-[100%] text-lg border-0 px-3 focus:outline-none focus:outline-[1px] focus:outline-peach"
+          placeholder="Last name"
+          {...register("lastName")}
+        />
+        {errors.lastName && (
+          <span className="absolute bottom-[-1.5em]">
+            {errors.lastName.message}
+          </span>
+        )}
+      </div>
+      <div className="relative flex flex-col input-group w-[80%]">
+        <input
+          type="text"
+          className="text-new-gray rounded-md min-h-9 w-[100%] text-lg border-0 px-3 focus:outline-none focus:outline-[1px] focus:outline-peach"
           placeholder="Username"
           {...register("username")}
         />
@@ -149,7 +187,10 @@ export default function RegisterForm({ ptSerif }: { ptSerif: NextFont }) {
           </span>
         )}
       </div>
-      <button disabled={isLoading} className="relative flex flex-col w-[80%] py-1 text-xl rounded-md border-new-peach border-[1px] duration-150 bg-new-peach text-new-gray after:content-[''] after:absolute after:bottom-[-1em] after:block after:h-[1px] after:bg-gray-200 after:w-[100%] enabled:hover:border-new-mint disabled:cursor-default">
+      <button
+        disabled={isLoading}
+        className="relative flex flex-col w-[80%] py-1 text-xl rounded-md border-new-peach border-[1px] duration-150 bg-new-peach text-new-gray after:content-[''] after:absolute after:bottom-[-1em] after:block after:h-[1px] after:bg-gray-200 after:w-[100%] enabled:hover:border-new-mint disabled:cursor-default"
+      >
         {isLoading ? (
           <div className="flex flex-row justify-center gap-2 p-1.5">
             <div className="w-3 h-3 rounded-full bg-white animate-bounce"></div>
