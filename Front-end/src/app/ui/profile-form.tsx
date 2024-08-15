b3ddad31d5276528972653604182b9aea3d4c308
PetarPetrov01 <petyr_p@mail.bg>
2024-08-15 19:31:45 +0300

    Extract upload form from the profile form

--- Front-end/src/app/ui/profile-form.tsx
+++ Front-end/src/app/ui/profile-form.tsx
@@ -8,6 +8,7 @@ import { useRouter } from "next/navigation";
 import { ChangeEvent, useRef, useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { z } from "zod";
+import UploadImageForm from "./upload-form";
 
 const ImageSchema = z.object({
   image: z
@@ -28,78 +29,23 @@ type Inputs = z.infer<typeof ImageSchema>;
 
 export default function ProfileForm({ user }: { user: User }) {
   const [selectedImage, setSelectedImage] = useState<null | string>(null);
-
-  const imageInputRef = useRef<HTMLInputElement | null>(null);
-  const formRef = useRef<HTMLFormElement>(null);
-
-  const router = useRouter();
+  const [showImageModal, setShowImageModal] = useState(false);
 
   const handleImageClick = () => {
-    imageInputRef.current?.click();
+    setShowImageModal(true);
+    document.body.style.overflow = 'hidden';
   };
-
-  const onImageChange = (event: ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files && event.target.files.length > 0) {
-      const file = event.target.files[0];
-      console.log(file.type);
-      setSelectedImage(file.name);
-      setValue("image", file);
-    }
-  };
-
-  console.log("IMAGE");
   console.log(user?.image);
-
-  const {
-    handleSubmit,
-    formState: { errors, dirtyFields },
-    reset,
-    register,
-    setValue,
-  } = useForm<Inputs>({
-    resolver: zodResolver(ImageSchema),
-  });
-
-  const { ref, ...rest } = register("image");
-
-  const onClose = () => {
-    setSelectedImage(null);
-    reset();
-  };
-
-  const processSubmit: SubmitHandler<Inputs> = async (data) => {
-    if (formRef.current) {
-      //If new FormData(formRef.current) is used, the file size appears to be 0 !?
-      const formData = new FormData();
-      formData.append("image", data.image);
-      try {
-        const response = await fetch("http://localhost:3001/api/upload/image", {
-          method: "POST",
-          body: formData,
-          credentials: "include",
-        });
-
-        const result = await response.json();
-        router.refresh();
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  };
 
   return (
     <>
       <h2 className="mt-4 text-new-peach text-center">{user.firstName} {user.lastName}</h2>
-      <form
-        action=""
-        ref={formRef}
-        encType="multipart/form-data"
-        onSubmit={handleSubmit(processSubmit)}
-        className="flex flex-col items-center sm:items-start sm:flex-row justify-between sm:gap-5 w-full"
+      <div
+                className="flex flex-col items-center sm:items-start sm:flex-row justify-between sm:gap-5 w-full"
       >
         <article className="w-[60%]  sm:w-[35%] relative flex flex-col items-center gap-4 py-10">
           <div className="relative w-[95%] flex flex-col bg-new-teal pt-28 p-16 rounded-2xl min-h-[20vh]">
-            <div className="absolute top-[-35%] sm:top-[-25%] left-[50%] translate-x-[-50%] rounded-full border-4 border-new-peach shadow-md shadow-new-sandstone">
+            <div onClick={handleImageClick} className="absolute top-[-35%] sm:top-[-25%] left-[50%] translate-x-[-50%] rounded-full border-4 border-new-peach shadow-md shadow-new-sandstone">
               <Image
                 src={user?.image ? user.image : "/default-company-pic.png"}
                 alt="profile pic"
@@ -108,39 +54,15 @@ export default function ProfileForm({ user }: { user: User }) {
                 className="rounded-full aspect-square object-cover"
               />
               <a className="absolute bottom-1 right-1 cursor-pointer rounded-full bg-new-gray text-new-peach p-[0.4rem] duration-150 hover:bg-new-teal">
-                <CameraIcon height={20} width={20} onClick={handleImageClick} />
+                <CameraIcon height={20} width={20}/>
               </a>
-              <input
-                type="file"
-                className="hidden"
-                ref={(e) => {
-                  ref(e);
-                  imageInputRef.current = e;
-                }}
-                {...rest}
-                onChange={onImageChange}
-              />
+              
             </div>
             <h3>{user.username}</h3>
             <h3>{user.email}</h3>
             <h3>phone</h3>
           </div>
-          {selectedImage && (
-            <div className="flex gap-6 items-center border-b-[1px] border-lightblue">
-              <p>{selectedImage}</p>
-              <a
-                className="cursor-pointer hover:text-lightblue"
-                onClick={onClose}
-              >
-                <XMarkIcon height={30} />
-              </a>
-            </div>
-          )}
-          {selectedImage && errors?.image && (
-            <span className="absolute bottom-[-1.5em] mt-1  text-sm italic text-pink ">
-              {errors.image.message}
-            </span>
-          )}
+  
         </article>
         <article className="w-[85%] sm:w-[60%] sm:py-10">
           <div className="bg-new-teal rounded-2xl p-10">
@@ -155,7 +77,8 @@ export default function ProfileForm({ user }: { user: User }) {
             </button>
           </div>
         )}
-      </form>
+      </div>
+      {showImageModal && <UploadImageForm setShowImageModal={setShowImageModal}/>}
     </>
   );
 }
---
+++ Front-end/src/app/ui/upload-form.tsx
@@ -0,0 +1,152 @@
+import { XMarkIcon } from "@heroicons/react/20/solid";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { ChangeEvent, Dispatch, SetStateAction, useRef, useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
+
+const ImageSchema = z.object({
+  image: z
+    .instanceof(File)
+    .refine((file) => /jpg|png|jpeg/.test(file.name.toLocaleLowerCase()), {
+      message: "File type not allowed",
+    })
+    .refine(
+      (file) => /image\/png|image\/jpeg|image\/gif|image\/webp/.test(file.type),
+      { message: "This file is not allowed" }
+    )
+    .refine((file) => file.size <= 1024 * 1024 * 5, {
+      message: "File is too large",
+    }),
+});
+
+type Inputs = z.infer<typeof ImageSchema>;
+
+export default function UploadImageForm({
+  setShowImageModal,
+}: {
+  setShowImageModal: Dispatch<SetStateAction<boolean>>;
+}) {
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const imageInputRef = useRef<HTMLInputElement | null>(null);
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const handleUploadImage = () => {
+    imageInputRef.current?.click();
+  };
+
+  const onImageChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files.length > 0) {
+      const file = event.target.files[0];
+      setSelectedImage(file.name);
+      setValue("image", file);
+    }
+  };
+
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+    reset,
+    setValue,
+  } = useForm<Inputs>({
+    resolver: zodResolver(ImageSchema),
+  });
+
+  const { ref, ...rest } = register("image");
+
+  const handleCloseModal = () => {
+    setShowImageModal(false);
+    document.body.style.overflow = "unset";
+  };
+
+  const handleCancel = () => {
+    setSelectedImage(null);
+    reset();
+  };
+
+  const processSubmit: SubmitHandler<Inputs> = async (data) => {
+    setIsLoading(true);
+    if (formRef.current && data.image instanceof File) {
+      const formData = new FormData();
+      formData.append("image", data.image);
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 2000);
+      console.log(formData);
+    }
+  };
+
+  return (
+    <div className="fixed  left-0 top-0 z-50 flex h-[100vh] w-[100vw] items-center justify-center bg-gray-200/40">
+      <div className="text-new-teal rounded-lg bg-new-mint p-8">
+        <div className="mb-4 flex justify-between gap-14">
+          <div></div>
+          <h2>Change profile image</h2>
+          <a
+            className="relative bottom-4 left-4 w-8 cursor-pointer text-red-500 duration-150 hover:text-red-600"
+            onClick={handleCloseModal}
+          >
+            <XMarkIcon width={50} className="w-full" />
+          </a>
+        </div>
+        <form
+          onSubmit={handleSubmit(processSubmit)}
+          ref={formRef}
+          className="flex flex-col items-center gap-4"
+        >
+          <a
+            onClick={handleUploadImage}
+            className="cursor-pointer border-2 border-dashed border-new-teal hover:bg-new-teal hover:text-new-mint duration-150 p-2"
+          >
+            {selectedImage ? `Choose another image` : "Upload image"}
+          </a>
+
+          <input
+            type="file"
+            className="hidden"
+            ref={(e) => {
+              ref(e);
+              imageInputRef.current = e;
+            }}
+            onChange={onImageChange}
+          />
+          {selectedImage && (
+            <>
+              <div className="relative flex gap-2 items-center">
+                {errors?.image && (
+                  <span className="absolute bottom-[-1em] mt-1 text-center text-sm italic text-new-sandstone ">
+                    {errors.image.message}
+                  </span>
+                )}
+                <p>{selectedImage}</p>
+                <a
+                  onClick={handleCancel}
+                  className="text-red-400 cursor-pointer hover:text-red-500 duration-150"
+                >
+                  <XMarkIcon width={24} />
+                </a>
+              </div>
+              <button
+                type="submit"
+                disabled={isLoading}
+                className="bg-new-peach w-20 h-10 enabled:hover:bg-new-sandstone duration-150"
+              >
+                {isLoading ? (
+                  <div className="flex flex-row justify-center gap-2">
+                    <div className="w-3 h-3 rounded-full bg-white animate-fast-bounce"></div>
+                    <div className="w-3 h-3 rounded-full bg-white animate-fast-bounce [animation-delay:-.15s]"></div>
+                    <div className="w-3 h-3 rounded-full bg-white animate-fast-bounce [animation-delay:-.3s]"></div>
+                  </div>
+                ) : (
+                  <p className="text-xl">Save</p>
+                )}
+              </button>
+            </>
+          )}
+        </form>
+      </div>
+    </div>
+  );
+}
