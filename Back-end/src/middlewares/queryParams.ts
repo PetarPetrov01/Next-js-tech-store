c709006a435ed69a74363f8e4c1fe311889bbd39
PetarPetrov01 <petyr_p@mail.bg>
2024-09-02 16:01:22 +0300

    implement queryParams middleware

---
+++ Back-end/src/middlewares/queryParams.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from "express";
+
+export default function queryParams(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  if (Object.keys(req.query).length > 0) {
+    if (req.query.sort) {
+      const [key, order] = (req.query.sort as string).split(":");
+      req.query.sort = {
+        key,
+        order,
+      };
+    }
+
+    if (req.query.price) {
+      const [lte, gte] = (req.query.price as string).split(":");
+      req.query.price = { lte, gte };
+    }
+  }
+
+  next();
+}
