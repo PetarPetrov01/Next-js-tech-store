759f277d62e462abea41e41f28dbd467b2e9a054
PetarPetrov01 <petyr_p@mail.bg>
2024-09-02 15:04:55 +0300

    Modify products fetch

--- Back-end/src/services/productService.ts
+++ Back-end/src/services/productService.ts
@@ -2,9 +2,19 @@ import { prisma } from "../config/db-config";
 
 async function getProducts() {
   const prods = await prisma.product.findMany({
-    include: { category: { select: { name: true } } },
+    include: {
+      category: { select: { name: true } },
+      brand: { select: { name: true } },
+      images: { select: { url: true } },
+    },
   });
-  return prods.map((p)=>({...p,category: p.category.name}));
+
+  return prods.map((p) => ({
+    ...p,
+    category: p.category.name,
+    brand: p.brand.name,
+    images: p.images.map((i) => i.url),
+  }));
 }
 
 export default { getProducts };
