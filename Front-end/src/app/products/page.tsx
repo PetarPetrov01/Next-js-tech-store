bbc614ecc6f6bb018092423128d4724ac8b8280d
PetarPetrov01 <petyr_p@mail.bg>
2024-09-03 20:03:47 +0300

    Render products

--- Front-end/src/app/products/page.tsx
+++ Front-end/src/app/products/page.tsx
@@ -43,12 +43,12 @@ export default async function Page({searchParams}: {searchParams: any}) {
         <div className="container max-w-[1450px] flex justify-center py-5 px-4">
           <div className="flex w-full justify-between items-start">
             <article className="sm:w-[28%] lg:w-[24%] flex flex-col border-2 border-new-peach min-h-60"></article>
-            <article className="sm:w-[71%] lg:w-[75%] border-2 border-new-mint flex flex-col items-center justify-center">
+            <article className="sm:w-[71%] lg:w-[75%] flex flex-col items-center justify-center">
               <h2 className="text-4xl">Browse our products</h2>
-              <div className="flex flex-wrap justify-start items-stretch md:gap-[2%] px-2">
-                {new Array(8).fill(1).map((id, index) => (
-                  <ProductCard prod={mockProd} key={id + index} />
-                ))}
+              <div className="flex flex-wrap w-full justify-start items-stretch md:gap-[2%] px-2">
+              {prods.map((prod,i)=>(
+                  <ProductCard prod={prod} key={prod.id} />
+              ))}
               </div>
             </article>
           </div>
--- Front-end/src/app/ui/product-card.tsx
+++ Front-end/src/app/ui/product-card.tsx
@@ -14,10 +14,11 @@ const router = useRouter();
       className="flex flex-col sm:w-[49%] md:w-[32%] lg:w-[23.5%] justify-start gap-2 mt-8 border-[1px] p-3 rounded-lg  bg-new-mint shadow-s"
     >
       <div className="flex w-full justify-center items-center overflow-hidden self-center">
-        <Image src={prod.images[0]} alt="Iph" width={100}  height={100} className="w-[90%]"/>
+        <Image src={prod.images[0]} alt={prod.name} width={100}  height={100} className="w-[90%]"/>
       </div>
       <div>
-        <h2 className="text-3xl">{prod.name}</h2>
+        <h2 className="text-2xl text-new-gray">{prod.brand} {prod.name}</h2>
+        <h2 className="text-2xl text-new-gray">${prod.price}</h2>
       </div>
       <h2>{prod.brand}</h2>
       <p>{prod.description} </p>
