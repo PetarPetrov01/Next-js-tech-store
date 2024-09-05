09eaa6b94e3c132c6e90f6cff31d91b849b32a22
PetarPetrov01 <petyr_p@mail.bg>
2024-09-05 20:18:01 +0300

    feat/FE: category filtering through searchParams

--- Front-end/src/app/components/CategoryFilter.tsx
+++ Front-end/src/app/components/CategoryFilter.tsx
@@ -1,9 +1,35 @@
 "use client";
+
+import { Categories } from "@/types/Product";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
 export default function CategoryFilter({
+  categories,
+}: {
+  categories: Categories;
 }) {
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  const searchParams = useSearchParams();
+
+  const handleFilterChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const { name, value } = e.target;
+    const params = new URLSearchParams(searchParams);
+
+    if (Number(value) == 0) {
+      params.delete(name);
+      replace(`${pathname}?${params.toString()}`);
+    } else {
+      params.set(name, value);
+      replace(`${pathname}?${params.toString()}`);
+    }
+  };
+
   return (
     <select
+      onChange={handleFilterChange}
       name="category"
+      defaultValue={searchParams.get("category")?.toString()}
       className="text-new-gray p-1"
     >
       <option value={0} className="text-new-gray">
@@ -12,6 +38,7 @@ export default function CategoryFilter({
       {categories &&
         categories.map((cat) => (
           <option key={cat.id} value={cat.id} className="text-new-gray">
+            {cat.name}
           </option>
         ))}
     </select>
