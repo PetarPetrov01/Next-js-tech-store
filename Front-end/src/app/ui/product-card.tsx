5c18ad10cb5eaf5499b2bd3cc5fa37171cce5b20
PetarPetrov01 <petyr_p@mail.bg>
2024-10-07 10:47:36 +0300

    feat/FE: Product form

--- Front-end/src/app/ui/product-card.tsx
+++ Front-end/src/app/ui/product-card.tsx
@@ -11,7 +11,7 @@ const router = useRouter();
     <div
       onClick={() => router.push(`/products/${prod.id}`)}
       key={prod.id}
-      className="flex flex-col sm:w-[49%] md:w-[32%] lg:w-[23.5%] justify-start gap-2 mt-8 border-[1px] p-3 rounded-lg  bg-new-mint shadow-s"
+      className="flex flex-col sm:w-[49%] md:w-[32%] lg:w-[23.5%] justify-start gap-2 mt-8 p-3 rounded-lg  bg-[#d1e8e2d2] shadow-s cursor-pointer hover:bg-new-mint duration-100"
     >
       <div className="flex w-full justify-center items-center overflow-hidden self-center">
         <Image src={prod.images[0]} alt={prod.name} width={100}  height={100} className="w-[90%]"/>
