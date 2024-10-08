eb0de8a7743e5d34e5f7f15fb88c529c402ebc49
PetarPetrov01 <petyr_p@mail.bg>
2024-10-08 19:48:00 +0300

    feat/FE: Cart products component

--- Front-end/src/app/cart/page.tsx
+++ Front-end/src/app/cart/page.tsx
@@ -1,3 +1,4 @@
+import CartOrder from "../components/ui/cart-order";
 import CartProducts from "../components/ui/cart-products";
 
 export default function CartPage() {
@@ -5,10 +6,9 @@ export default function CartPage() {
     <main className="bg-gradient-radial min-h-[75vh] from-new-teal to-new-gray">
       <div className="container max-w-[1450px] flex justify-center m-auto p-5">
         <section className="flex flex-col gap-10 mt-6 w-full">
-          <h1 className="w-full">Shopping cart</h1>
           <div className="flex w-full justify-between gap-8 items-start">
             <CartProducts />
-            <article className="flex-[1_1_25%] bg-new-teal-100 min-h-10"></article>
+            <CartOrder />
           </div>
         </section>
       </div>
--- Front-end/src/app/components/ui/cart-products.tsx
+++ Front-end/src/app/components/ui/cart-products.tsx
@@ -1,19 +1,31 @@
 "use client";
 
 import useCartStore from "@/app/store/cart";
-import { MinusIcon, PlusIcon, TrashIcon } from "@heroicons/react/24/solid";
+import {
+  MinusIcon,
+  PlusIcon,
+  TrashIcon,
+  XCircleIcon,
+} from "@heroicons/react/24/solid";
 import Image from "next/image";
 import { useState } from "react";
-import EraseProductDiaolog from "./cart-erase-product-diaolog";
+import EraseProductDiaolog from "./cart-erase-product-dialog";
+import { HeartIcon } from "@heroicons/react/24/outline";
+import ClearCartDialog from "./clear-cart-dialog";
 
 export default function CartProducts() {
-  const { cart, clearCart, increaseQuantity, decreaseQuantity } =
-    useCartStore();
+  const { cart, increaseQuantity, decreaseQuantity } = useCartStore();
 
   const [eraseProduct, setEraseProduct] = useState<{
     productId: string;
     name: string;
   } | null>(null);
+
+  const [showClearDialog, setShowClearDialog] = useState(false);
+
+  const totalProducts = () => {
+    return cart.reduce((acc, prod) => acc + prod.quantity, 0);
+  };
 
   const handleIncrease = (
     productId: string,
@@ -44,61 +56,85 @@ export default function CartProducts() {
   };
 
   return (
-    <article className="flex-[1_1_70%] flex flex-col gap-6">
-      {cart.map((prod) => (
-        <div
-          key={prod.id}
-          className="min-h-8 flex justify-between bg-new-teal-90 rounded-md"
-        >
-          <div className="flex-[1_1_18%] flex justify-center items-center p-2 aspect-square">
-            <Image
-              src={prod.images[0]}
-              alt={prod.name}
-              width={300}
-              height={300}
-              className="object-cover"
-            />
-          </div>
-          <div className="flex-[1_1_70%] flex p-6 pt-8">
-            <div className="flex-[1_1_70%] flex flex-row justify-between">
-              <h2>{prod.name}</h2>
-            </div>
-            <div className="flex-[1_1_25%] flex flex-col items-end gap-2">
-              <h3>${prod.price * prod.quantity}</h3>
-              <div className="flex gap-3 text-lg px-3 py-1 rounded-sm bg-gray-800/75">
-                <button
-                  onClick={() =>
-                    handleDecrease(prod.id, prod.quantity, prod.name)
-                  }
-                  className="hover:text-new-peach-90 duration-200"
-                >
-                  <MinusIcon width={18} height={18} />
-                </button>
-                <span>{prod.quantity}</span>
-                <button
-                  onClick={() =>
-                    handleIncrease(prod.id, prod.stock, prod.quantity)
-                  }
-                  className="hover:text-new-peach-90 duration-200"
-                >
-                  <PlusIcon width={18} height={18} />
-                </button>
-              </div>
-              <div
-                onClick={() => handleEraseClick(prod.id, prod.name)}
-                className="flex gap-1 text-red-300 hover:text-red-400 cursor-pointer duration-100s"
-              >
-                <span>Remove</span>
-                <TrashIcon height={20} width={20} />
-              </div>
-            </div>
-          </div>
+    <article className="flex-[1_1_70%] flex flex-col">
+      <header className="relative flex justify-between items-stretch bg-new-teal-100 p-4 pb-3 rounded-t-md after:absolute after:w-[96%] after:bottom-0 after:right-[2%] after:bg-gray-200/50 after:h-[1px]">
+        <div className="flex justify-between gap-7 items-end">
+          <h1 className="text-3xl">Cart</h1>
+          <p className="text-white/60 pb-[2px]">Products: {totalProducts()}</p>
         </div>
-      ))}
+        <button
+          onClick={() => setShowClearDialog(true)}
+          className="flex gap-1 items-center mr-2 text-red-400 hover:text-red-500/90 duration-200"
+        >
+          <p>Clear cart</p>
+          <XCircleIcon width={25} height={25} />
+        </button>
+      </header>
+      <div className="flex flex-col gap-6">
+        {cart.map((prod) => (
+          <div
+            key={prod.id}
+            className="min-h-8 flex justify-between bg-new-teal-100 rounded-md first:rounded-t-none"
+          >
+            <div className="flex-[1_1_17%] flex justify-center items-center p-2 aspect-square">
+              <Image
+                src={prod.images[0]}
+                alt={prod.name}
+                width={300}
+                height={300}
+                className="object-cover"
+              />
+            </div>
+            <div className="flex-[1_1_70%] flex p-6 pt-8">
+              <div className="flex-[1_1_70%] flex flex-col gap-4">
+                <h2>{prod.name}</h2>
+                <span>in stock: {prod.stock}</span>
+              </div>
+              <div className="flex-[1_1_25%] flex flex-col items-end gap-3">
+                <h3>${prod.price * prod.quantity}</h3>
+                <div className="flex gap-4 text-lg px-3 py-1 rounded-sm bg-gray-800/75">
+                  <button
+                    onClick={() =>
+                      handleDecrease(prod.id, prod.quantity, prod.name)
+                    }
+                    className="hover:text-new-peach-90 duration-200"
+                  >
+                    <MinusIcon width={18} height={18} />
+                  </button>
+                  <span>{prod.quantity}</span>
+                  <button
+                    onClick={() =>
+                      handleIncrease(prod.id, prod.stock, prod.quantity)
+                    }
+                    className="hover:text-new-peach-90 duration-200"
+                  >
+                    <PlusIcon width={18} height={18} />
+                  </button>
+                </div>
+                <div className="flex gap-1 items-center cursor-pointer">
+                  <span>Add to wish list</span>
+                  <HeartIcon height={22} width={22} />
+                </div>
+                <div
+                  onClick={() => handleEraseClick(prod.id, prod.name)}
+                  className="flex gap-1 text-red-400 hover:text-red-500/90 cursor-pointer duration-100"
+                >
+                  <span>Remove</span>
+                  <TrashIcon height={20} width={20} />
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
       <EraseProductDiaolog
         open={eraseProduct ? true : false}
         setEraseProduct={setEraseProduct}
         eraseProduct={eraseProduct}
+      />
+      <ClearCartDialog
+        showClearDialog={showClearDialog}
+        setShowClearDialog={setShowClearDialog}
       />
     </article>
   );
