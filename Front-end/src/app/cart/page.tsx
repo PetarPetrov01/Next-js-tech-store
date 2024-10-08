cb80686ab7f1e2dd081cc3b68a947468b1a31333
PetarPetrov01 <petyr_p@mail.bg>
2024-10-08 17:34:39 +0300

    feat/FE: Cart page

---
+++ Front-end/src/app/cart/page.tsx
@@ -0,0 +1,15 @@
+
+export default function CartPage() {
+  return (
+    <main className="bg-gradient-radial min-h-[75vh] from-new-teal to-new-gray">
+      <div className="container max-w-[1450px] flex justify-center m-auto p-5">
+        <section className="flex flex-col gap-10 mt-6 w-full">
+          <h1 className="w-full">Shopping cart</h1>
+          <div className="flex w-full justify-between gap-8 items-start">
+            <article className="flex-[1_1_25%] bg-new-teal-100 min-h-10"></article>
+          </div>
+        </section>
+      </div>
+    </main>
+  );
+}
