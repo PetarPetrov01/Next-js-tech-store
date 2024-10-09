36bf1cd803bb80621e9f38991d591803e9d8a16b
PetarPetrov01 <petyr_p@mail.bg>
2024-10-09 11:09:36 +0300

    style/FE: Pulse animation on cart change

--- Front-end/src/app/components/ui/cart.tsx
+++ Front-end/src/app/components/ui/cart.tsx
@@ -2,9 +2,24 @@
 
 import useCartStore from "@/app/store/cart";
 import { ShoppingCartIcon } from "@heroicons/react/24/solid";
+import { useEffect, useState } from "react";
 
 export default function Cart() {
   const { cart } = useCartStore();
+
+  const [isPulsing, setIsPulsing] = useState(false);
+
+  useEffect(() => {
+    if (cart.length > 0) {
+      setIsPulsing(true);
+
+      const timeout = setTimeout(() => {
+        setIsPulsing(false);
+      }, 300);
+
+      return () => clearTimeout(timeout);
+    }
+  }, [cart]);
 
   const cartQuantity = () => {
     const totalLengh = cart.reduce((acc, { quantity }) => acc + quantity, 0);
@@ -13,12 +28,14 @@ export default function Cart() {
 
   return (
     <div className="relative p-2">
-      <ShoppingCartIcon width={30} height={40} />
-      {cart.length > 0 && (
-        <div className="absolute top-2 right-1 flex justify-center items-center text-new-gray text-[0.8rem] font-bold bg-new-peach-100 rounded-full w-5 h-5">
-          {cartQuantity()}
-        </div>
-      )}
+      <ShoppingCartIcon width={35} height={35} />
+      <div
+        className={`absolute top-2 right-1 flex justify-center items-center text-new-gray text-[0.8rem] font-bold bg-new-peach-100 rounded-full scale-100 duration-300 ${
+          cart.length > 0 ? "w-5 h-5" : "w-0 h-0 top-3 right-1/2"
+        } ${isPulsing && 'scale-125'}`}
+      >
+        {cart.length > 0 && cartQuantity()}
+      </div>
     </div>
   );
 }
