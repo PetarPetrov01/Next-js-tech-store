511a70db3b4dbc0ecf34e34d9b25f89c9788fd5e
PetarPetrov01 <petyr_p@mail.bg>
2024-10-12 15:54:14 +0300

    feat/BE: Handle multiple images upload to multer

--- Back-end/src/utils/multerValidation.ts
+++ Back-end/src/utils/multerValidation.ts
@@ -1,20 +1,20 @@
 import multer, { FileFilterCallback } from "multer";
 import path from "path";
+import { fileURLToPath } from "url";
+import { CustomRequest } from "../middlewares/sesssion";
+import { NextFunction, Response } from "express";
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
     cb(null, path.join(__dirname, "..", "..", "uploads/"));
   },
   filename(req, file, cb) {
-    const date = new Date().toISOString().replace(/:/g, '-');
+    const date = new Date().toISOString().replace(/:/g, "-");
     cb(null, `${date}-${file.originalname.toLowerCase()}`);
   },
 });
 
-function verifyFileType(
-  file: Express.Multer.File,
-  cb: FileFilterCallback
-) {
+function verifyFileType(file: Express.Multer.File, cb: FileFilterCallback) {
   const filetypes = /jpeg|jpg|png|gif/;
   const extension = filetypes.test(
     path.extname(file.originalname).toLocaleLowerCase()
@@ -24,16 +24,24 @@ function verifyFileType(
   if (extension && mimetype) {
     cb(null, true);
   } else {
-    cb(new Error ("Wrong file format"));
+    cb(new Error("Wrong file format"));
   }
 }
 
-const upload = multer({
+const options: multer.Options = {
   storage: storage,
   limits: { fileSize: 1024 * 1024 * 50 },
   fileFilter(req, file, cb) {
     verifyFileType(file, cb);
   },
-});
+};
 
-export default upload;
+const multipleOptions: multer.Options = {
+  ...options,
+  limits: { fileSize: options.limits?.fileSize, files: 6 },
+};
+
+const uploadSingle = multer(options).single("image");
+const uploadMultiple = multer(multipleOptions).array("image");
+
+export { uploadSingle, uploadMultiple };
