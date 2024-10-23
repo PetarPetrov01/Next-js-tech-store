41289455a71aa337902cd945c143313d4d20cd81
PetarPetrov01 <petyr_p@mail.bg>
2024-10-23 14:21:34 +0300

    feat/FE: Implement product images scrolling

--- Front-end/src/app/components/ProductImages.tsx
+++ Front-end/src/app/components/ProductImages.tsx
@@ -2,15 +2,56 @@
 
 import useWindowWidth from "@/hooks/useWindowWidth";
 import Image from "next/image";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+
+const IMAGE_CONTAINER_WIDTH_IN_PERCENT = 23.5/100;
+const IMAGES_GAP_IN_PERCENT = 2/100;
 
 export default function ProductImages({ images }: { images: string[] }) {
   const [displayImageIndex, setDisplayImageIndex] = useState(0);
+  const sliderRef = useRef<HTMLDivElement>(null);
+
+  const totalImages = images.length;
+  const scrollPages = Math.ceil(totalImages / 4);
+  const [currentScrollPage, setCurrentScrollPage] = useState(1);
+
   const { windowWidth } = useWindowWidth();
 
   const changeImageIndex = (index: number) => {
     setDisplayImageIndex(index);
+  };
+
+  const scrollRight = () => {
+    if (sliderRef.current) {
+      const sliderWidth = sliderRef.current.clientWidth;
+      sliderRef.current.scrollBy({
+        left: sliderWidth + (IMAGES_GAP_IN_PERCENT * sliderWidth),
+      });
+      setCurrentScrollPage((prev) => prev + 1);
+    }
+  };
+
+  const scrollLeft = () => {
+    if (sliderRef.current) {
+      const sliderWidth = sliderRef.current?.clientWidth;
+      let scrollAmount: number;
+
+      if (currentScrollPage == scrollPages) {
+        const lastPageItemsCount = totalImages % 4;
+
+        scrollAmount =
+          ((IMAGE_CONTAINER_WIDTH_IN_PERCENT + IMAGES_GAP_IN_PERCENT) *
+            sliderWidth) *
+          lastPageItemsCount;
+      } else {
+        scrollAmount = sliderWidth + (IMAGES_GAP_IN_PERCENT * sliderWidth);
+      }
+
+      console.log(scrollAmount);
+      sliderRef.current?.scrollBy({ left: -scrollAmount });
+      setCurrentScrollPage((prev) => prev - 1);
+    }
   };
 
   console.log(images);
@@ -27,10 +68,19 @@ export default function ProductImages({ images }: { images: string[] }) {
         />
       </div>
       <div className="relative flex justify-center w-full">
-        <button className="absolute left-[4%] top-1/2 -translate-y-1/2 rounded-full bg-transparent p-2 duration-200 hover:bg-neutral-600">
-          <IoIosArrowBack size={"1.3em"} />
-        </button>
-        <div className="flex gap-[2%] overflow-x-scroll w-[80%] scrollbar-hide">
+        {currentScrollPage > 1 && (
+          <button
+            onClick={scrollLeft}
+            className="absolute left-[4%] top-1/2 -translate-y-1/2 rounded-full bg-transparent p-2 duration-200 hover:bg-neutral-600"
+          >
+            <IoIosArrowBack size={"1.3em"} />
+          </button>
+        )}
+        <div
+          id="slider"
+          ref={sliderRef}
+          className="flex gap-[2%] overflow-x-scroll w-[80%] scrollbar-hide scroll-smooth"
+        >
           {images.map((image, i) => (
             <div
               key={i}
@@ -52,9 +102,14 @@ export default function ProductImages({ images }: { images: string[] }) {
             </div>
           ))}
         </div>
-        <button className="absolute right-[4%] top-1/2 -translate-y-1/2 rounded-full bg-transparent p-2 duration-200 hover:bg-neutral-600">
-          <IoIosArrowForward size={"1.3em"} />
-        </button>
+        {currentScrollPage < scrollPages && (
+          <button
+            onClick={scrollRight}
+            className="absolute right-[4%] top-1/2 -translate-y-1/2 rounded-full bg-transparent p-2 duration-200 hover:bg-neutral-600"
+          >
+            <IoIosArrowForward size={"1.3em"} />
+          </button>
+        )}
       </div>
     </div>
   );
