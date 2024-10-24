d788ae4a6f189aecab50cb3a8f68b0018cff573a
PetarPetrov01 <petyr_p@mail.bg>
2024-10-24 14:44:16 +0300

    feat/FE: Validate product post fields

--- Front-end/src/app/components/ui/post-product-form.tsx
+++ Front-end/src/app/components/ui/post-product-form.tsx
@@ -8,6 +8,8 @@ import { useEffect, useMemo, useRef, useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
+import { IoWarning } from "react-icons/io5";
+
 export default function PostProductForm({
   categories,
 }: {
@@ -22,11 +24,25 @@ export default function PostProductForm({
 
   type Inputs = z.infer<typeof postProductSchema>;
 
-  const { formState, register, watch } = useForm<Inputs>({
-    resolver: zodResolver(createProductSchema(categories)),
+  const {
+    formState: { dirtyFields, errors, isValid },
+    register,
+    watch,
+    handleSubmit,
+  } = useForm<Inputs>({
+    resolver: zodResolver(postProductSchema),
+    // defaultValues: {
+    //   brandId: 0,
+    //   categoryId: 0,
+    //   model: "",
+    //   price: 0,
+    //   stock: 0,
+    //   description: "",
+    // },
   });
 
   const watchedCatId = watch("categoryId");
+  const wathedBrandId = watch("brandId");
 
   useEffect(() => {
     const fetchBrands = async () => {
@@ -36,14 +52,26 @@ export default function PostProductForm({
     fetchBrands();
   }, [watchedCatId]);
 
+  const processSubmit = handleSubmit(async (data) => {
+    console.log(data);
+    console.log(errors);
+    console.log(isValid);
+  });
+
   return (
-    <form ref={formRef} className="w-1/2 flex flex-col gap-4">
+    <form
+      onSubmit={processSubmit}
+      ref={formRef}
+      className="w-1/2 flex flex-col gap-6"
+    >
       <div className="w-full flex justify-between gap-10">
         <h2 className="basis-[30%]">Category:</h2>
         <div className="flex justify-between gap-4 basis-[65%]">
           <select
             {...register("categoryId")}
-            className="bg-transparent border-[1px] basis-[50%] px-4 border-new-mint"
+            className={`bg-transparent border-[1px] basis-[50%] px-4 ${
+              watchedCatId != 0 ? "border-new-peach-100" : "border-new-mint"
+            }`}
             defaultValue={0}
           >
             <option value={0} disabled className="text-new-darkblue text-xl">
@@ -55,7 +83,9 @@ export default function PostProductForm({
               </option>
             ))}
           </select>
-          <button>Add another category</button>
+          <button className="border-[1px] border-new-mint px-4">
+            Add new category
+          </button>
         </div>
       </div>
       <div className="w-full flex justify-between gap-10">
@@ -63,10 +93,12 @@ export default function PostProductForm({
         <div className="flex justify-between gap-4 basis-[65%]">
           <select
             {...register("brandId")}
-            className="bg-transparent border-[1px] basis-[50%] px-4 border-new-mint"
+            className={`bg-transparent border-[1px] basis-[50%] px-4 ${
+              wathedBrandId != 0 ? "border-new-peach-100" : "border-new-mint"
+            }`}
             defaultValue={0}
           >
-            <option value={0} disabled>
+            <option value={0} disabled className="text-new-darkblue text-xl">
               Choose brand
             </option>
             {brands.length &&
@@ -74,46 +106,89 @@ export default function PostProductForm({
                 <option
                   key={brand.id}
                   value={brand.id}
-                  //   className="text-new-darkblue"
+                  className="text-new-darkblue"
                 >
                   {brand.name}
                 </option>
               ))}
           </select>
-          <button>Add another brand</button>
+          <button className="border-[1px] border-new-mint px-4">
+            Add new brand
+          </button>
         </div>
       </div>
       <div className="w-full flex justify-between gap-10">
         <h2 className="basis-[30%]">Model: </h2>
-        <div className="relative basis-[65%] bg-new-mint p-[1px] after:absolute after:top-0 after:left-0 after:w-0 after:h-full after:duration-700 after:ease-in-out after:bg-new-peach-100 after:z-0 focus-within:after:w-full">
+        <div
+          className={`relative basis-[65%] bg-new-mint p-[1px] after:absolute after:top-0 after:left-0 after:w-0 after:h-full after:duration-700 after:ease-in-out after:bg-new-peach-100 after:z-0 focus-within:after:w-full ${
+            dirtyFields.model && "after:w-full"
+          } `}
+        >
           <input
             type="text"
             {...register("model")}
             placeholder="Model"
             className="relative w-full h-full bg-new-darkblue outline-none px-4 z-20"
           />
+          {errors.model && (
+            <>
+              <span className="absolute text-[0.9em] bottom-[-1.5em] left-1 text-red-400">
+                {errors.model.message}
+              </span>
+              <IoWarning
+                size={"1.4em"}
+                className="absolute top-1/2 -translate-y-1/2 right-2 z-20 text-red-400"
+              />
+            </>
+          )}
         </div>
       </div>
       <div className="w-full flex justify-between gap-10">
         <h2 className="basis-[30%]">Price: </h2>
         <div className="relative basis-[65%] bg-new-mint p-[1px] after:absolute after:top-0 after:left-0 after:w-0 after:h-full after:duration-700 after:ease-in-out after:bg-new-peach-100 after:z-0 focus-within:after:w-full">
           <input
-            type="text"
-            {...register("price")}
+            {...register("price", {
+              valueAsNumber: true,
+            })}
+            type="number"
             placeholder="Price"
             className="relative w-full h-full bg-new-darkblue outline-none px-4 z-20"
           />
+          {errors.price && (
+            <>
+              <span className="absolute text-[0.9em] bottom-[-1.5em] left-1 text-red-400">
+                {errors.price.message}
+              </span>
+              <IoWarning
+                size={"1.4em"}
+                className="absolute top-1/2 -translate-y-1/2 right-2 z-20 text-red-400"
+              />
+            </>
+          )}
         </div>
       </div>
       <div className="w-full flex justify-between gap-10">
         <h2 className="basis-[30%]">Stock: </h2>
         <div className="relative basis-[65%] bg-new-mint p-[1px] after:absolute after:top-0 after:left-0 after:w-0 after:h-full after:duration-700 after:ease-in-out after:bg-new-peach-100 after:z-0 focus-within:after:w-full">
           <input
-            type="text"
-            {...register("stock")}
+            type="number"
+            {...register("stock", {
+              valueAsNumber: true,
+            })}
             placeholder="Stock"
             className="relative w-full h-full bg-new-darkblue outline-none px-4 z-20"
           />
+          {errors.stock && (
+            <>
+              <span className="absolute text-[0.9em] bottom-[-1.5em] left-1 text-red-400">
+                {errors.stock.message}
+              </span>
+              <IoWarning
+                size={"1.4em"}
+                className="absolute top-1/2 -translate-y-1/2 right-2 z-20 text-red-400"
+              />
+            </>
+          )}
         </div>
       </div>
       <div className="w-full flex items-stretch justify-between gap-10">
@@ -124,8 +199,20 @@ export default function PostProductForm({
             placeholder="Description"
             className="relative w-full bg-new-darkblue min-h-20 max-h-[200px] outline-none px-4 z-20"
           />
+          {errors.description && (
+            <>
+              <span className="absolute text-[0.9em] bottom-[-1.5em] left-1 text-red-400">
+                {errors.description.message}
+              </span>
+              <IoWarning
+                size={"1.4em"}
+                className="absolute top-1/2 -translate-y-1/2 right-2 z-20 text-red-400"
+              />
+            </>
+          )}
         </div>
       </div>
+      <button type="submit">POST</button>
     </form>
   );
 }
--- Front-end/src/zodSchemas/postProductSchema.ts
+++ Front-end/src/zodSchemas/postProductSchema.ts
@@ -15,12 +15,23 @@ export default function createProductSchema(
       message: "Invalid category",
     }),
     brandId: z.number(),
-    model: z.string().trim().min(1, { message: "Model is required" }),
-    price: z.number().min(1, { message: "Price is required" }),
+    model: z
+      .string()
+      .trim()
+      .min(1, { message: "Model is required" })
+      .min(3, { message: "Model must be atleast 3 characters long" })
+      .max(15, { message: "Model must no more than 15 characters long" }),
+    price: z
+      .number({ message: "Must be a number" })
+      .min(1, { message: "Price can't be a negative number" }),
+    stock: z
+      .number({ message: "Must be a number" })
+      .min(1, { message: "Stock can't be a negative number" }),
     description: z
       .string()
       .trim()
-      .min(1, { message: "Description is required" }),
-    stock: z.number().min(1, { message: "Stock is required" }),
+      .min(1, { message: "Description is required" })
+      .min(30, { message: "Description must be alteast 30 characters long" })
+      .max(250, { message: "Description no more than 250 characters long" }),
   });
 }
