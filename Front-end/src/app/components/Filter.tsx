62de871215fce47c19238ead65d3cfe12ad1a1a0
PetarPetrov01 <petyr_p@mail.bg>
2024-11-04 09:59:20 +0200

    refactor/FE: Fetch brands and products on client side
    This approach reduces renders on change of searchParams. The previous approach would get the params from the page.tsx and pass theb down to ProdcutsLayout which passes it ProductsList, causing every child to re-render including static data components

--- Front-end/src/app/components/Filter.tsx
+++ Front-end/src/app/components/Filter.tsx
@@ -1,14 +1,20 @@
+import { Categories } from "@/types/Product";
 import { getCategories } from "../lib/data";
 import CategoryFilter from "./CategoryFilter";
 import PriceFilter from "./PriceFilter";
+import BrandFilterWrapper from "./BrandFilterWrapper";
 
-export default async function Filter({}:{}) {
-  const categories = await getCategories();
-
+export default async function Filter({
+  categories,
+}: {
+  categories: Categories;
+}) {
+  //Make the whole Filter a client component and recieve cats only, brands will be fetched on FE
   return (
     <div className="flex flex-col gap-4">
       <CategoryFilter categories={categories} />
       <PriceFilter />
+      <BrandFilterWrapper />
     </div>
   );
 }
--- Front-end/src/app/components/ProductsList.tsx
+++ Front-end/src/app/components/ProductsList.tsx
@@ -1,19 +1,35 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import { getProds } from "../lib/data";
 import ProductCard from "./product/product-card";
+import { PopulatedProduct } from "@/types/Product";
+import { useSearchParams } from "next/navigation";
 
-export default async function ProductsList({
-  searchParams,
-}: {
-  searchParams: any;
-}) {
-  const prods = await getProds(searchParams);
+export default function ProductsList() {
+  const [products, setProducts] = useState<PopulatedProduct[]>([]);
+  const searchParams = useSearchParams();
 
-  const viewType = searchParams.view || 'grid'
+  useEffect(() => {
+    const fetchProds = async () => {
+      const prods = await getProds(searchParams);
+      setProducts(prods);
+    };
+    fetchProds();
+  }, [searchParams]);
+
+  const viewType = searchParams.get("view") || "grid";
 
   return (
-    <div className={`flex flex-wrap w-full mt-6  ${viewType == 'grid' ? 'justify-start items-stretch md:gap-[2%]' : 'flex-col gap-6'}`}>
-      {prods?.length > 0 ? (
-        prods.map((prod, i) => <ProductCard prod={prod} key={prod.id} />)
+    <div
+      className={`flex flex-wrap w-full mt-6  ${
+        viewType == "grid"
+          ? "justify-start items-stretch gap-[4%] md:gap-[2%]"
+          : "flex-col gap-6"
+      }`}
+    >
+      {products?.length > 0 ? (
+        products.map((prod, i) => <ProductCard prod={prod} key={prod.id} />)
       ) : (
         <h2>No products!</h2>
       )}
