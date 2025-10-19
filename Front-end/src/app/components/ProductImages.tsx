be55570d67b37ddd22fad5b9a60872d675bd487c
PetarPetrov01 <petar.petrovv2001@gmail.com>
2025-10-19 15:20:56 +0300

    feat/FE: Prettier import order plugin; Swiper

--- Front-end/src/app/components/ProductImages.tsx
+++ Front-end/src/app/components/ProductImages.tsx
@@ -1,48 +1,37 @@
-"use client";
+'use client'
 
-import useWindowWidth from "@/hooks/useWindowWidth";
-import { useMemo } from "react";
-import MobileProductImages from "./product/mobile-product-images";
-import { usePathname } from "next/navigation";
-import { useAuthContext } from "@/contexts/AuthProvider";
-import DesktopProductImages from "./product/desktop-product-images";
-import useMounted from "@/hooks/useMounted";
-import { ProductImageSkeleton } from "./ui/loaders/skeletons";
+import { usePathname } from 'next/navigation'
+import { useMemo } from 'react'
 
-export default function ProductImages({
-  images,
-  ownerId,
-}: {
-  images: string[];
-  ownerId: string;
-}) {
-  const { user } = useAuthContext();
-  const pathname = usePathname();
-  const hasMounted = useMounted();
+import { useAuthContext } from '@/contexts/AuthProvider'
+import useMounted from '@/hooks/useMounted'
+import useWindowWidth from '@/hooks/useWindowWidth'
 
-  const isOwner = useMemo(() => user?.id == ownerId, [ownerId, user?.id]);
+import { ProductImageSkeleton } from '@/components/ui/loaders/skeletons'
 
-  const { windowWidth } = useWindowWidth();
+import DesktopProductImages from './product/desktop-product-images'
+import MobileProductImages from './product/mobile-product-images'
+
+export default function ProductImages({ images, ownerId }: { images: string[]; ownerId: string }) {
+  const { user } = useAuthContext()
+  const pathname = usePathname()
+  const hasMounted = useMounted()
+
+  const isOwner = useMemo(() => user?.id == ownerId, [ownerId, user?.id])
+
+  const { windowWidth } = useWindowWidth()
 
   return (
     <div className="relative w-full flex flex-col items-center gap-6 overflow-hidden">
       {hasMounted ? (
         windowWidth < 640 ? (
-          <MobileProductImages
-            isOwner={isOwner}
-            images={images}
-            pathname={pathname}
-          />
+          <MobileProductImages isOwner={isOwner} images={images} pathname={pathname} />
         ) : (
-          <DesktopProductImages
-            isOwner={isOwner}
-            images={images}
-            pathname={pathname}
-          />
+          <DesktopProductImages isOwner={isOwner} images={images} pathname={pathname} />
         )
       ) : (
         <ProductImageSkeleton />
       )}
     </div>
-  );
+  )
 }
